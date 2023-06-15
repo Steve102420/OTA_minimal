1d4582b823016d18009c7fd7991ffe3776898b1e
Steve102420 <62448442+Steve102420@users.noreply.github.com>
2023-06-15 20:44:12 +0200

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,6 @@
+//Credentials
+const char* ssid = "********";
+const char* password = "********"; 
+
+const char* fwImageURL = "http://********/firmware.bin";
+const char* fwVersionURL = "http://********/version.h";
